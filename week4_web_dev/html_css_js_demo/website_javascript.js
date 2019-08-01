418e68e9001e4f6311b153336e48435519675ed2
Akriti Singh <akritisingh@berkeley.edu>
2019-08-01 12:56:33 -0400

    added slideshow js

--- week4_web_dev/html_css_js_demo/website_javascript.js
+++ week4_web_dev/html_css_js_demo/website_javascript.js
@@ -7,3 +7,16 @@ function bring_back() {
   document.getElementById("change_color").style.opacity = "1.0";
   document.getElementById("change_color").style.transition = "1s";
 }
+
+var current_image;
+var i = 0;
+var images = ["puppies/pic1.jpg", "puppies/pic2.jpg", "puppies/pic3.jpg", "puppies/pic4.jpg"];
+
+function show_images() {
+	i = i + 1;
+	if (i >= images.length) {
+		i = 0;
+	}
+	current_image = images[i];
+	document.getElementById("slideshow").src = current_image;
+}
